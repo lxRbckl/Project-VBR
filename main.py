6276c77005c79b1878144816711d72dec66ea8e7
ala2q6 <lxrbckl@protonmail.com>
2022-09-06 19:59:49 -0500

    backup

---
+++ main.py
@@ -0,0 +1,171 @@
+# Project RB8 by Alex Arbuckle #
+
+
+# import <
+import dash_bootstrap_components as dbc
+from dash import dcc, html, Dash, Input, State, Output, dash_table
+
+# >
+
+
+# global <
+gFeature = {
+
+    'rev' : {
+
+        'min' : 0,
+        'max' : 50,
+        'value' : 20,
+        'name' : '# Revolution'
+
+    },
+    'spin' : {
+
+        'min' : 0,
+        'max' : 5,
+        'value' : 3,
+        'name' : '# Spin'
+
+    }
+
+}
+application = Dash(
+
+    external_stylesheets = [dbc.themes.GRID]
+
+)
+
+# >
+
+
+def layoutFunction():
+    '''  '''
+
+    return dbc.Container(
+
+        children = dbc.Row(
+
+            justify = 'between',
+            children = [
+
+                # input <
+                # datatable <
+                # graph <
+                dbc.Row(
+
+                    justify = 'between',
+                    style = dict(margin = '2.5%'),
+                    children = [
+
+                        dbc.Col(
+
+                            width = 'auto',
+                            children = [
+
+                                # label <
+                                # input <
+                                dbc.Label(
+
+                                    style = dict(fontSize = 15),
+                                    children = gFeature[k]['name']
+
+                                ),
+                                dbc.Input(
+                                    step = 1,
+                                    id = f'{k}Id',
+                                    type = 'number',
+                                    min = gFeature[k]['min'],
+                                    max = gFeature[k]['max'],
+                                    value = gFeature[k]['value'],
+                                    style = dict(padding = '3%', fontSize = 15)
+
+                                )
+
+                                # >
+
+                            ]
+
+                        )
+
+                    for k in gFeature.keys()]
+
+                ),
+                dbc.Col(
+
+                    width = 'auto',
+                    align = 'center',
+                    id = 'colDatatableId'
+
+                ),
+                dbc.Col(
+
+                    width = 'auto',
+                    align = 'center',
+                    id = 'colGraphId'
+
+                )
+
+                # >
+
+            ]
+
+        )
+
+    )
+
+
+@application.callback(
+
+    Output('colDatatableId', 'children'),
+    Input('revId', 'value'),
+    Input('spinId', 'value')
+
+)
+def datatableCallback(pRevValue: int, pSpinValue: int):
+    '''  '''
+
+    d = {
+
+        'Revolution' : [(r + 1) for r in range(pRevValue)],
+        **{f'Spin {s + 1}' : [None for r in range(pRevValue)] for s in range(pSpinValue)}
+
+    }
+
+    return dash_table.DataTable(
+
+        editable = True,
+        id = 'datatableId',
+        row_deletable = True,
+        style_data = dict(textAlign = 'center'),
+        style_header = dict(paddingLeft = 15, paddingRight = 15),
+        data = [{k : d[k][i] for k in d.keys()} for i in range(pRevValue)],
+        columns = [{'id' : k, 'name' : k, 'deletable' : True} for k in d.keys()]
+
+    )
+
+
+@application.callback(
+
+    Output('colGraphId', 'children'),
+    Input('datatableId', 'data')
+
+)
+def graphCallback(pDatatableData: list):
+    '''  '''
+
+    print(pDatatableData)
+
+    # return dcc.Graph(
+    #
+    #     #
+    #
+    # )
+
+
+# main <
+if (__name__ == '__main__'):
+
+    application.layout = layoutFunction()
+    application.run_server(debug = True)
+
+# >
